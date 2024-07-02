2bbb39832fa21f14cf220a54793d61171fe1c58d
OS <OS@LAPTOP-0FH11JO7>
2024-07-02 08:11:51 +0700

    Add project files.

---
+++ Nhom4QuanLyBanThucPham.Module/BusinessObjects/ORMDataModel1Code/NguoiQuanLy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using DevExpress.Persistent.Base;
+namespace Nhom4QuanLyBanThucPham.Module.BusinessObjects.ORMDataModel1
+{
+    [DefaultClassOptions]
+    [NavigationItem("Hệ Thống")]
+    public partial class NguoiQuanLy
+    {
+        public NguoiQuanLy(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
---
+++ Nhom4QuanLyBanThucPham.Module/BusinessObjects/ORMDataModel1Code/Nhacungcap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using DevExpress.Persistent.Base;
+namespace Nhom4QuanLyBanThucPham.Module.BusinessObjects.ORMDataModel1
+{
+    [DefaultClassOptions]
+    [NavigationItem("Hệ Thống")]
+    public partial class Nhacungcap
+    {
+        public Nhacungcap(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
